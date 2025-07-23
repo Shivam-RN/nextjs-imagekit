82a0726f3d4a89876f3274f26d667cc493bb30a7
Shivam-RN <shivamrana1809@gmail.com>
2025-07-23 20:12:34 +0530

    builing components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import Providers from "./components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Providers>
         {children}
-        <Toaster/>
+        </Providers>
       </body>
     </html>
   );
